c5b20803144c6b5423ad51ca72329857aaeb5616
FlyingMedusa <sleboda.m98@gmail.com>
2019-12-25 22:39:30 +0100

    update

--- 01_binary_search_algorithm.py
+++ 01_binary_search_algorithm.py
@@ -1,10 +1,10 @@
-def binary_search(list, item):
+def binary_search(user_list, item):
     low = 0
-    high = len(list) - 1
+    high = len(user_list) - 1
 
     while low <= high:
         mid = (low + high) // 2
-        guess = list[mid]
+        guess = user_list[mid]
 
         if guess == item:
             return mid
@@ -15,8 +15,9 @@ def binary_search(list, item):
 
     return None
 
+
 my_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25]
 
 chosen_num = int(input("Give an odd number from 1 to 25 and you'll get it's index:\t"))
-print(chosen_num,"- index:", binary_search(my_list, chosen_num))
+print(chosen_num, "- index:", binary_search(my_list, chosen_num))
 
